55a46683573e05de2dc854be97ca0fca7f564000
Shawn-McSorley <22706094@student.uwa.edu.au>
2025-03-24 13:39:55 +0800

    Added phase noise and modulation plots

--- software-phase-locked-loops-1.py
+++ software-phase-locked-loops-1.py
@@ -56,7 +56,6 @@ plt.xlabel('Frequency [Hz]')
 plt.ylabel('Power Spectrum [V**2/Hz]')
 plt.xlim([fc-5e3, fc+5e3])
 plt.savefig('Shawn-McSorley.github.io/assets/carrier_power_density.png', dpi=200, transparent=True)
-plt.show()
 
 # generate analytic signal
 analytic_signal = signal.hilbert(carrier)
@@ -67,23 +66,73 @@ phase_down_shifted = np.angle(down_shifted)
 
 nperseg = 2**15
 f1, Pxx1 = signal.welch(random_noise, fs, nperseg=nperseg)
-f2, Pxx2 = signal.welch(instantaneous_phase, fs, nperseg=nperseg)
 f3, Pxx3 = signal.welch(phase_down_shifted, fs, nperseg=nperseg)
-
 scaled_additive_noise_PSD = 2 * Pxx1 / A**2
 
-
-plt.figure()
+plt.figure(figsize=(10, 8))
 plt.plot(f1, Pxx1, label='Generated additive noise')
 plt.plot(f1, scaled_additive_noise_PSD, label='Scaled additive noise')
-plt.plot(f2, Pxx2, label='Instantaneous phase')
 plt.plot(f3, Pxx3, label='Phase from down shifted')
 plt.axvline(x=fc, color='k', linestyle='--', label='Carrier frequency')
 plt.legend()
 plt.yscale('log')
 plt.xscale('log')
 plt.xlabel('Frequency [Hz]')
+plt.ylabel('Power Spectrum [unit**2/Hz]')
+plt.ylim([1e-11, 1e-8])
+plt.savefig('Shawn-McSorley.github.io/assets/phase_noise_PSD.png', dpi=200, transparent=True)
+
+test_modulation = 0.1 * np.sin(2*np.pi*100*t) # A slow phase modulation
+A1 = 1
+A2 = 0.01
+carrier1 = A1*np.cos(2*np.pi*fc*t + test_modulation) + random_noise
+carrier2 = A2*np.cos(2*np.pi*fc*t + test_modulation) + random_noise
+
+analytic_signal1 = signal.hilbert(carrier1)
+analytic_signal2 = signal.hilbert(carrier2)
+
+down_shifted1 = np.angle(analytic_signal1 * np.exp(-1j*2*np.pi*fc*t))
+down_shifted2 = np.angle(analytic_signal2 * np.exp(-1j*2*np.pi*fc*t))
+
+nperseg = 2**15
+f1, Pxx1 = signal.welch(down_shifted1, fs, nperseg=nperseg)
+f2, Pxx2 = signal.welch(down_shifted2, fs, nperseg=nperseg)
+
+plt.figure(figsize=(10, 8))
+plt.plot(f1, Pxx1, label=f'A = {A1}')
+plt.plot(f2, Pxx2, label=f'A = {A2}')
+plt.legend()
+plt.yscale('log')
+plt.xscale('log')
+plt.xlabel('Frequency [Hz]')
 plt.ylabel('Power Spectrum [rad**2/Hz]')
+plt.ylim([1e-9, 2e-3])
+plt.xlim([0, fc])
+plt.savefig('Shawn-McSorley.github.io/assets/phase_noise_PSD_modulation.png', dpi=200, transparent=True)
+
+test_modulation = 10 * np.sin(2*np.pi*10*t) # A slow phase modulation
+A1 = 1
+A2 = 0.05
+carrier1 = A1*np.cos(2*np.pi*fc*t + test_modulation) + random_noise
+carrier2 = A2*np.cos(2*np.pi*fc*t + test_modulation) + random_noise
+
+analytic_signal1 = signal.hilbert(carrier1)
+analytic_signal2 = signal.hilbert(carrier2)
+
+down_shifted1 = np.unwrap(np.angle(analytic_signal1 * np.exp(-1j*2*np.pi*fc*t)))
+down_shifted2 = np.unwrap(np.angle(analytic_signal2 * np.exp(-1j*2*np.pi*fc*t)))
+
+plt.figure()
+plt.plot(t, down_shifted2, label=f'A = {A2}')
+plt.plot(t, down_shifted1, label=f'A = {A1}')
+plt.legend(loc = 'upper right')
+plt.xlabel('Time [s]')
+plt.ylabel('Phase [rad]')
+plt.xlim([0, 0.4])
+plt.savefig('Shawn-McSorley.github.io/assets/carrier_modulation.png', dpi=200, transparent=True)
+plt.show()
+
+
 
 # Why does the phase noise PSD cutoff at the carrier frequency?
 f1, Pxx1 = signal.welch(carrier, fs, nperseg=nperseg, return_onesided=False) # We will use the two-sided PSD to compare to the down-shifted analytic signal
